857be090cac96d790ed0dcf8fadf44ebfa89844d
Juan Lumaca <lumacajuanmanuel@gmail.com>
2024-05-28 23:24:42 -0300

    Models y migraciones

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -8,3 +8,22 @@ class Estudiante(models.Model):
     apellido = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
     telefono = models.CharField(max_length=30)
+
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=30)
+    comision = models.IntegerField()
+
+class Profesor(models.Model):
+    documento = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=30)
+    profesion = models.CharField(max_length=30)
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=30)
+    fecha_entrega = models.DateField()
+    entregado = models.BooleanField()
