a2389b39072e9a0deb08ef89ab7821ec1346182e
Juan Lumaca <lumacajuanmanuel@gmail.com>
2024-05-31 09:38:49 -0300

    Estilos a los API Form, arreglos visuales

--- AppCoder/forms.py
+++ AppCoder/forms.py
@@ -1,26 +1,136 @@
 from django import forms
 
+
 class EstudianteFormulario(forms.Form):
-    documento = forms.CharField()
-    nombre = forms.CharField()
-    apellido = forms.CharField()
-    email = forms.CharField()
-    telefono = forms.CharField()
+    documento = forms.CharField(
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Documento',  # Placeholder como en el form HTML
+        })
+    )
+    nombre = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Nombre',  # Placeholder para nombre
+        })
+    )
+    apellido = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Apellido',  # Placeholder para apellido
+        })
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Email',  # Placeholder para email
+        })
+    )
+    telefono = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Teléfono',  # Placeholder para teléfono
+        })
+    )
+
+    submit = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'type': 'submit',
+            'class': 'btn btn-block btn-bold btn-primary',
+            'value': 'Guardar'
+        })
+    )
 
 class CursoFormulario(forms.Form):
-    nombre = forms.CharField()
-    comision = forms.IntegerField()
+    nombre = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Nombre del curso',  # Placeholder para teléfono
+        })
+
+    )
+    comision = forms.IntegerField(
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Número de comisión',  # Placeholder para teléfono
+        })
+    )
+
+    submit = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'type': 'submit',
+            'class': 'btn btn-block btn-bold btn-primary',
+            'value': 'Guardar'
+        })
+    )
 
 
 class ProfesorFormulario(forms.Form):
-    documento = forms.CharField()
-    nombre = forms.CharField()
-    apellido = forms.CharField()
-    email = forms.CharField()
-    telefono = forms.CharField()
-    curso = forms.CharField()
+    documento = forms.CharField(
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Documento',  # Placeholder para teléfono
+        })
+
+    )
+    nombre = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Nombre',  # Placeholder para teléfono
+        })
+
+    )
+    apellido = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Apellido',  # Placeholder para teléfono
+        })
+    )
+    email = forms.CharField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Correo electrónico',  # Placeholder para teléfono
+        })
+    )
+    telefono = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Teléfono',  # Placeholder para teléfono
+        }))
+    curso = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Curso',  # Placeholder para teléfono
+        })
+    )
+
+    submit = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'type': 'submit',
+            'class': 'btn btn-block btn-bold btn-primary',
+            'value': 'Guardar'
+        })
+    )
 
 class EntregableFormulario(forms.Form):
-    nombre = forms.CharField()
+    nombre = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase CSS
+            'placeholder': 'Nombre',  # Placeholder para teléfono
+        })
+    )
     fecha_de_entrega = forms.DateField(widget=forms.DateInput(attrs={'type':'date','class': 'form-control', 'placeholder': 'Fecha de Entrega'}))
-    entregado = forms.BooleanField()
+    entregado = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(attrs={
+            'class': 'form-check-input',
+        })
+    )
+
+    submit = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'type': 'submit',
+            'class': 'btn btn-block btn-bold btn-primary',
+            'value': 'Guardar'
+        })
+    )
